3ddf6045bd7210ca4b97d12cce0983e37d2e2c73
Aaron Delgado <radees.24.16@gmail.com>
2022-03-28 09:24:52 -0500

    comit encima del primer commit remoto

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angul-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Project_Angular';
+}
